4a98d435c5c17264cc7ab88ce2c639fffe4ad295
kindrew <kindrew99@gmail.com>
2023-12-04 16:25:43 +0000

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Blog = require("./models/blogs");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //connect to mongodb
 const dbURI =
